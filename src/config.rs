8ac0dd898437fc669309968800948fbf1a6f7055
nicholas-roether <nicholas-roether@t-online.de>
2024-10-16 10:00:50 +0200

    Refactoring in config.rs

--- src/config.rs
+++ src/config.rs
@@ -1,4 +1,4 @@
-use std::{fs, path::PathBuf, str::FromStr, sync::Arc};
+use std::{fs, path::PathBuf, str::FromStr};
 
 use log::error;
 use serde::Deserialize;
@@ -14,7 +14,7 @@ pub struct Config {
     pub server: ServerConfig,
 }
 
-pub fn read_config(path: Option<PathBuf>) -> Arc<Config> {
+pub fn read_config(path: Option<PathBuf>) -> Config {
     let path: PathBuf = path.unwrap_or_else(|| PathBuf::from_str(DEFAULT_CONFIG_PATH).unwrap());
     let contents = match fs::read_to_string(&path) {
         Ok(contents) => contents,
@@ -23,15 +23,15 @@ pub fn read_config(path: Option<PathBuf>) -> Arc<Config> {
                 "Failed to read config at {}: {err}. Reverting to default config.",
                 path.display()
             );
-            return Arc::new(Config::default());
+            return Config::default();
         }
     };
 
     match toml::from_str(&contents) {
-        Ok(config) => Arc::new(config),
+        Ok(config) => config,
         Err(err) => {
             error!("Failed to parse config file: {err}. Reverting to default config.");
-            Arc::new(Config::default())
+            Config::default()
         }
     }
 }
